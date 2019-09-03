e75064bbd3357842ae69a6c71629cd121101ada3
Edgar Lopez-Garcia <edgarylop@gmail.com>
2019-09-02 22:30:38 -0700

    Add xbox joystick story + input dispatcher

---
+++ Assets/PlatformTilt/Scripts/ControllerUpdateService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStories;
+
+public class ControllerUpdateService : MonoBehaviour
+{
+    public StoriesHelper storiesHelper;
+    private Vector2 JoystickLeftValue = Vector2.zero;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 joystickLeft = Vector2.zero;
+        joystickLeft.x = Input.GetAxis("Horizontal");
+        joystickLeft.y = Input.GetAxis("Vertical");
+        if (!Vector2.Equals(joystickLeft, JoystickLeftValue))
+        {
+            // Only update if joystick value has changed
+            JoystickLeftValue.x = joystickLeft.x;
+            JoystickLeftValue.y = joystickLeft.y;
+            storiesHelper.Dispatch(XBox360ControllerStory.SetJoystickLeftFactory.Get(JoystickLeftValue));
+        }
+    }
+}
---
+++ Assets/PlatformTilt/Scripts/XBox360ControllerStory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStories;
+
+[CreateAssetMenu(menuName="Unity Stories/PlatformTilt/Xbox360Controller Story")]
+public class XBox360ControllerStory : Story
+{
+    public Vector2 JoystickLeft = Vector2.zero;
+    public Vector2 JoystickRight = Vector2.zero;
+
+    public override void InitStory()
+    {
+        JoystickLeft = Vector2.zero;
+        JoystickRight = Vector2.zero;
+    }
+
+    // Actions / Factories
+    public class SetJoystickLeft : GenericAction<XBox360ControllerStory, Vector2>
+    {
+        public override void Action(XBox360ControllerStory story, Vector2 value)
+        {
+            // Avoids using new Vector3 to not produce garbage
+            story.JoystickLeft.x = value.x;
+            story.JoystickLeft.y = value.y;
+        }
+    }
+    public static GenericFactory<SetJoystickLeft, XBox360ControllerStory, Vector2> SetJoystickLeftFactory = new GenericFactory<SetJoystickLeft, XBox360ControllerStory, Vector2>();
+
+    public class SetJoystickRight : GenericAction<XBox360ControllerStory, Vector2>
+    {
+        public override void Action(XBox360ControllerStory story, Vector2 value)
+        {
+            // Avoids using new Vector3 to not produce garbage
+            story.JoystickRight.x = value.x;
+            story.JoystickRight.y = value.y;
+        }
+    }
+}
